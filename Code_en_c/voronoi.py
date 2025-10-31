2e344425930fceb6bc8012e8a6cae397cd228150
piloup12i <lionel.peduzzi@student.uclouvain.be>
2025-10-31 17:41:45 +0100

    Update voronoi.py

--- Code_en_c/voronoi.py
+++ Code_en_c/voronoi.py
@@ -52,6 +52,7 @@ def circumcenter(triangle):
 
     return (Ux, Uy)
 
+
 def get_triangle_circumcenters(triangles):
     circumcenters = []
     for triangle in triangles:
@@ -60,7 +61,10 @@ def get_triangle_circumcenters(triangles):
         triangle.center = center
     return circumcenters
 
+
 def Delaunay_triangulation(pts):
+    if len(pts)<3:
+        return []
     write_points_to_file(pts[:, 0], pts[:, 1], input_file)
     res = lib.del2d_py(input_file, output_file)
     if res != 0:
@@ -77,6 +81,7 @@ def Delaunay_triangulation(pts):
         triangle.center = center
         triangles.append(triangle)
     return triangles
+
 
 def find_neighbors(triangles):
     """
@@ -125,10 +130,19 @@ ax.set_title("Cliquez pour ajouter des points (Voronoi Cells)")
 
 # structure mutable partagée entre callbacks permet de switcher du mode et ajouter des points
 data = {
-    'pts': np.array([(5.0, 2.0), (5.0, 8.0), (2.0, 5.0), (8.0, 5.0), (5.0, 4.0), (5.0, 6.0), (9.0, 1.0)]),
+    'pts': np.empty((0, 2)),
     # 'pts': np.array([(2.0, 2.0), (4.0, 2.0), (6.0, 2.0)]),
     'mode': 1  # 0: Delaunay, 1: Voronoi
 }
+p_x = [] ; p_y =[]
+def onclick(event):
+    if event.inaxes != ax:
+        return
+    new_pt = np.array((event.xdata, event.ydata))
+    data['pts'] = np.vstack([data['pts'], new_pt])
+    print(f"Point ajouté : ({event.xdata:.2f}, {event.ydata:.2f})")
+    update(ax, data)
+
 
 def update(ax, data):
     ax.clear()
@@ -142,7 +156,7 @@ def update(ax, data):
 
 
 def plot(triangles, pts, ax, mode, extend_length=1000):
-    if mode == 0:
+    if mode == 0 and len(pts) >= 3:
         # Dessin des triangles
         for triangle in triangles:
             arr_x = [p.x for p in triangle.points] + [triangle.points[0].x]
@@ -150,7 +164,16 @@ def plot(triangles, pts, ax, mode, extend_length=1000):
             ax.plot(arr_x, arr_y, 'b-')
             cx, cy = triangle.center
             ax.plot(cx, cy, 'ro')
-    else:
+    if len(pts)==2:
+        a = pts[0]
+        b = pts[1]
+        M = np.array([(a[0] + b[0])/2, (a[1] + b[1])/2])
+        v = np.array([b[0] - a[0], b[1] - a[1]])
+        v = v / np.linalg.norm(v)
+        n = np.array([-v[1], v[0]])
+        ax.scatter([p[0] for p in pts], [p[1] for p in pts], color='red')  # cell centers
+        plt.plot([M[0]-n[0] * 12, M[0] + n[0] * 12], [M[1]-n[1] * 12, M[1] + n[1] * 12], 'g--')
+    else : 
         # Segments de Voronoï (fini + infini)
         edge_dict = find_neighbors(triangles)
         for edge, tris in edge_dict.items():
@@ -168,7 +191,8 @@ def plot(triangles, pts, ax, mode, extend_length=1000):
                 if norm < 1e-12:
                     continue
                 n = n / norm
-                C = np.array(t.center)
+                C = np.array([(A[0] + B[0])/2, (A[1] + B[1])/2])
+                D = np.array(t.center)
 
                 # Sens du vecteur normal
                 # On le dirige vers l'extérieur du triangle. Le barycentre est le point moyen du nuage (intérieur au nuage)
@@ -178,22 +202,12 @@ def plot(triangles, pts, ax, mode, extend_length=1000):
                 to_center = barycenter - C
                 if np.dot(n, to_center) > 0:
                     n = -n
-                ax.plot([C[0], C[0] + n[0]*extend_length],
-                        [C[1], C[1] + n[1]*extend_length],
+                ax.plot([D[0], D[0] + n[0]*extend_length],
+                        [D[1], D[1] + n[1]*extend_length],
                         'r--')
 
     # pts
     ax.plot(pts[:, 0], pts[:, 1], 'ko')
-
-
-def onclick(event):
-    if event.inaxes != ax:
-        return
-    new_pt = np.array([[event.xdata, event.ydata]])
-    data['pts'] = np.vstack([data['pts'], new_pt])
-    # data['pts'] = data['pts'][np.lexsort((data['pts'][:,1], data['pts'][:,0]))]
-    print(f"Point ajouté : ({event.xdata:.2f}, {event.ydata:.2f})")
-    update(ax, data)
 
 
 def switch_mode(event):
@@ -209,7 +223,9 @@ def switch_mode(event):
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
 kid = fig.canvas.mpl_connect('key_press_event', switch_mode)
 
-update(ax, data)
+if len(data['pts'])>0:
+    update(ax, data)
+
 plt.show()
 
 # delete temp files
